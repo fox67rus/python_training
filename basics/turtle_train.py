c89907f9b4ff6ed3b33d63974487e07342b58ac1
Elena <fox67rus@mail.ru>
2022-12-10 14:10:47 +0300

    turtle train

--- basics/turtle_train.py
+++ basics/turtle_train.py
@@ -3,50 +3,62 @@ import turtle
 t = turtle.Turtle()
 t.shape('turtle')
 
-""" draws S
-t.forward(50)
-t.left(90)
-t.forward(50)
-t.left(90)
-t.forward(50)
-t.right(90)
-t.forward(50)
-t.right(90)
-t.forward(50)
-"""
-
-""" draws a square
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.left(90)
-t.forward(100)
-"""
-
-""" draw a circle 
-for i in range(120):
-    t.forward(3)
-    t.left(3)
-    i += 1
-"""
-
-""" draws a lot of square"""
-length = 10
-x = 0
-y = 0
-for i in range(5,25,5):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.forward(length)
+def draw_s():
+    """ draws S
+    """
+    t.forward(50)
     t.left(90)
-    t.forward(length)
+    t.forward(50)
     t.left(90)
-    t.forward(length)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(50)
+
+
+def draw_square():
+    """
+    draws a square
+    """
+    t.forward(100)
     t.left(90)
-    t.forward(length)
-    x -=i
-    y +=i
-    length += i*2
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+
+
+def draw_circle():
+    """
+        draw a circle
+    """
+    for i in range(120):
+        t.forward(3)
+        t.left(3)
+        i += 1
+
+
+def draw_many_square():
+    """ draws a lot of square"""
+    distance = 10
+    turtle.speed(1)
+
+    for i in range(0, 50, 5):
+        turtle.penup()
+        turtle.goto(-i, -i)
+        turtle.pendown()
+        turtle.forward(distance)
+        turtle.left(90)
+        turtle.forward(distance)
+        turtle.left(90)
+        turtle.forward(distance)
+        turtle.left(90)
+        turtle.forward(distance)
+        print(turtle.pos(), end="")
+        print(f"{distance =}")
+        distance += 10
+
+if __name__ == '__main__':
+    draw_many_square()
