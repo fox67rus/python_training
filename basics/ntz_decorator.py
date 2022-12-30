99266a65687295080dc966095da9b18a251e173e
Elena <fox67rus@mail.ru>
2022-12-30 17:06:51 +0300

    decorators complete

--- basics/ntz_decorator.py
+++ basics/ntz_decorator.py
@@ -1,17 +1,25 @@
-def decorator(function):
-   import time
-   def wrapper():
-        print(f'Запускаем функцию: {function}')
-        start = time.time()
-        function()
-        finish = time.time()
-        print(f'{function} завершила выполнение за {finish - start} секунд.')
+def decorator(count):
+    def dec(function):
+        import time
+        def wrapper(*args, **kwargs):
+            print(f'Запускаем функцию: {function}')
+            start = time.time()
+            for c in range(count):
+                result = function(*args, **kwargs)
+                print(f"{c} -{result}")
+            finish = time.time()
+            print(f"{function} завершила выполнение за {finish - start} секунд.")
+            return result
 
-    # возвращаем обертку
-    return wrapper
+        return wrapper
+    return dec
 
-@decorator
-def hello_world():
-    print('Hello World!')
 
-hello_world()
+@decorator(count=5)
+def hello_world(name):
+    hello_name = f'Hello, {name}'
+    return hello_name
+
+
+test = hello_world('Boris')
+print(test)
