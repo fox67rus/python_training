de0226e5646c4cab52ba2c05efa218cd752de48d
Elena <fox67rus@mail.ru>
2022-12-30 19:55:08 +0300

    Except - счетчик ввода

--- basics/ntz_except.py
+++ basics/ntz_except.py
@@ -1,8 +1,17 @@
-try:
-    age = int(input("Введите ваш возраст: "))
-    print(f"Вы ввели {age} лет")
-except:
-    print(f"Вы ввели некорректное значение")
+count = 0
+
+while count < 5:
+
+    try:
+        age = int(input("Введите ваш возраст: "))
+        print(f"Вы ввели {age} лет")
+        break
+    except ValueError as e:
+        print(f"Вы ввели некорректное значение - {e}")
+    except:
+        print("Неизвестная ошибка")
+
+    count +=1
 
 
 
