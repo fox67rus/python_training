ef267610bcc8804da186b585935c79a77b23ec37
Elena <fox67rus@mail.ru>
2022-12-30 14:08:32 +0300

    while and for

--- basics/while_for.py
+++ basics/while_for.py
@@ -1,3 +1,42 @@
+# перебор множеств
+os = frozenset({'Windows', 'macOS', 'Ubuntu', 'ChromeOS'})
+print(os)
+
+for o in os:
+    print(o)
+
+# перебор словаря
+d = {'FirstName': 'Boris', 'LastName': 'Ivanov', 'Age': 26}
+print(d)
+
+for k in d:
+    print(f"{k} - {d[k]}")
+
+for k, v in d.items():
+    print(f"{k} - {v}")
+
+for k in d.keys():
+    print(k)
+
+for v in d.values():
+    print(v)
+
+
+from collections import namedtuple
+
+names = ('Alex', 'Michael', 'Fedor')
+print(type(names))
+
+for n in names:
+    print(n)
+
+# перебор именованных кортежей
+Info = namedtuple('Information', 'Firstname, Lastname, Age, IsCoder')
+info = [Info('Boris', 'Ivanov', 26, True), Info('Alex', 'Petrov', 20, False)]
+
+for i in info:
+    print(i)
+
 digitals = range(20)
 print(digitals)
 
@@ -10,4 +49,4 @@ for d in digitals:
     if d > 10:
         break
 
-    print(d**2)
+    print(d ** 2)
