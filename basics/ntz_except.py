155f81c0945b0b448e24993dd67376f687fefb79
Elena <fox67rus@mail.ru>
2022-12-30 19:57:32 +0300

    Except - счетчик ввода

--- basics/ntz_except.py
+++ basics/ntz_except.py
@@ -3,15 +3,12 @@ count = 0
 while count < 5:
 
     try:
-        age = int(input("Введите ваш возраст: "))
-        print(f"Вы ввели {age} лет")
+        num = int(input("Введите первое число: "))
+        print(f"Вы ввели {num} лет")
         break
     except ValueError as e:
         print(f"Вы ввели некорректное значение - {e}")
     except:
         print("Неизвестная ошибка")
 
-    count +=1
-
-
-
+    count += 1
