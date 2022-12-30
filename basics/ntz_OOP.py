3be0b85c0c7d7f0adf510add812da8f1d0e508b8
Elena <fox67rus@mail.ru>
2022-12-30 19:32:48 +0300

    Base OOP encapsulation - add setter and getter

--- basics/ntz_OOP.py
+++ basics/ntz_OOP.py
@@ -1,23 +1,59 @@
 class House:
-
+    # константа - условная неизменяемая переменная
+    FOUNDATION = 'strip'
     roof = 'шифер'
+
     # конструктор
-    def __init__(self, square, floor, wall):
-        self.square = square
-        self.floor = floor
-        self.wall = wall
+    def __init__(self, floor):
+        self.__square = 15
+        # скрытый атрибут
+        self._floor = floor
+        # приватный атрибут
+        self.__wall = 'кирпич'
         print("Дом с заданными параметрами построен")
 
+    @property
+    def square(self):
+        return self.__square
+
+    @square.setter
+    def square(self, set_square):
+        if set_square < 15:
+            self.__square = 15
+        elif set_square > 100:
+            self.__square = 100
+        else:
+            self.__square = set_square
+
+    @property
+    def wall(self):
+        return self.__wall
+
+    @wall.setter
+    def wall(self, wall):
+        self.__wall = wall
+
     def build_balcony(self, i):
-        self.square -= i
+        self.__square -= i
 
     def build_roof(self):
         print('Построили крышу с помощью материала - ' + self.roof)
 
 
-my_house = House(100, 3, 'кирпич')
+my_house = House(3)
+my_house.wall = 'брус'
+print(my_house.wall)
+
+my_house.square = 100
 print(my_house.square)
-my_house.build_balcony(10)
-print(my_house.square)
-print(type(my_house))
-my_house.build_roof()
+
+# # обращение к скрытому атрибуту
+# print(my_house._wall)
+# # обрщение к приватному атрибуту
+# print(my_house._House__floor)
+
+# print(my_house.square)
+# my_house.build_balcony(10)
+# print(my_house.square)
+# print(type(my_house))
+# my_house.build_roof()
