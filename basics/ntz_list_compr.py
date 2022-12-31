561e365fa723a1c4729def5d4ac22a6ef508b8c4
Elena <fox67rus@mail.ru>
2022-12-31 13:11:39 +0300

    list comprehension - замена отрицательных элементов на 0

--- basics/ntz_list_compr.py
+++ basics/ntz_list_compr.py
@@ -17,7 +17,7 @@
 # print(result)
 
 # # перебор списка кортежей
-source = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+# source = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 # result = []
 #
 # for col_sources in source:
@@ -26,6 +26,12 @@ source = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 #
 # print(result)
 
-# с помощью list comprehension
-result = [el_sources for col_sources in source for el_sources in col_sources]
+# # с помощью list comprehension
+# result = [el_sources for col_sources in source for el_sources in col_sources]
+# print(result)
+
+# замена отрицательных элементов на 0
+source = [30, 15, -5, 7, 15, -9, ]
+
+result = [s if s > 0 else 0 for s in source]
 print(result)
