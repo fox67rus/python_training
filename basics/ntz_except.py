85db9817c7c3099fd78ad5b655396f0edc456fa1
Elena <fox67rus@mail.ru>
2022-12-31 08:14:40 +0300

    Except - обработка ошибок

--- basics/ntz_except.py
+++ basics/ntz_except.py
@@ -3,12 +3,22 @@ count = 0
 while count < 5:
 
     try:
-        num = int(input("Введите первое число: "))
-        print(f"Вы ввели {num} лет")
-        break
+        num1 = int(input("Введите первое число: "))
+        num2 = int(input("Введите второе число: "))
+        result = num1/num2
+        # print(f"Вы ввели {num} лет")
+        # break
     except ValueError as e:
         print(f"Вы ввели некорректное значение - {e}")
+
+    except ZeroDivisionError as z:
+        print('Делить на ноль нельзя!')
+        print(f'{z}')
     except:
         print("Неизвестная ошибка")
+    # блок кода выполнится в любом случае - для закрытия файлов и освобождения ресурсов
+    finally:
+        print('finaly')
+
 
     count += 1
