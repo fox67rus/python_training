7b6fb5d1332106372566c260e04e77b40b7fbc29
Elena <fox67rus@mail.ru>
2022-12-31 12:08:21 +0300

    Data class - default values

--- basics/ntz_dataclass.py
+++ basics/ntz_dataclass.py
@@ -9,19 +9,24 @@
 # human = Human("Alex", "Ivanov", 26, 1996)
 #
 # print(human.lastname)
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 # data class
-@dataclass(frozen=True)
+@dataclass()
 class Human:
-    firstname: str
-    lastname: str
-    age: int
-    year: int
+    firstname: str = 'Ivan'
+    lastname: str = 'Petrov'
+    age: int = 20
+    year: int = 2002
+    # не инициилизировать и не получать значения
+    fullname: str = field(init=False, repr=False)
+
+    # формирование параметра после инициализации
+    def __post_init__(self):
+        self.fullname = self.lastname + " " + self.firstname
 
 
 human = Human("Alex", "Ivanov", 26, 1996)
-print(human)
-human.firstname = 'Ivan'
-print(human)
+print(human.fullname)
+
