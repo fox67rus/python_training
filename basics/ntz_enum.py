24cffc102034b0bb70cb42a69d50c5417305a8ab
Elena <fox67rus@mail.ru>
2022-12-31 08:42:50 +0300

    Перечисления-списки

--- basics/ntz_enum.py
+++ basics/ntz_enum.py
@@ -2,6 +2,20 @@
 from enum import Enum
 
 
+# перечисления - списки
+class Season(Enum):
+    Winter = ['January', 'February', 'December']
+    Spring = ['March', 'April', 'May']
+    Summer = ['June', 'July', 'August']
+    Autumn = ['September', 'October', 'November']
+
+
+for season in Season:
+    print(f'Время года: {season.name}')
+    for month in season.value:
+        print(f'Месяц: {month}')
+
+
 class Days(Enum):
     Monday = (1, 'Понедельник')
     Tuesday = (2, 'Вторник')
@@ -15,6 +29,24 @@ class Days(Enum):
         self.id = id
         self.russian = russian
 
+
+class Month(Enum):
+    January = 1  # Январь
+    February = 2  # Февраль
+    March = 3  # Март
+    April = 4  # Апрель
+    May = 5  # Май
+    June = 6  # Июнь
+    July = 7  # Июль
+    August = 8  # Август
+    September = 9  # Сентябрь
+    October = 10  # Октябрь
+    November = 11  # Ноябрь
+    December = 12  # Декабрь
+
+
+for month in Month:
+    print(f'{month.name} это {month.value} месяц.')
 
 for day in Days:
     print(f'{day.name} это {day.id} день недели. На русском - {day.russian}')
