360a0bfac70a91c406c05fe56c5d7ae508195c8c
Elena <fox67rus@mail.ru>
2023-01-01 11:03:19 +0300

    web parser - get title

---
+++ app/parser/web_parser.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+host = 'https://komp.1k.by/mobile-notebooks/'
+
+headers = {
+    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+}
+
+response_page = requests.get(url=host, headers=headers)
+
+bs = BeautifulSoup(response_page.text, 'lxml')
+
+print(bs.title.text)
+
