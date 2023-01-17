d0602631efb8d485ab369f7d26d14a1023619882
Elena <fox67rus@mail.ru>
2023-01-17 18:26:25 +0300

    Stepik

---
+++ course/stepik_pokolenie_1/unit1.py
@@ -0,0 +1,85 @@
+# Обработка цифр числа
+num = 754
+a = num % 10
+b = (num % 100) // 10
+c = num // 100
+print(a)
+print(b)
+print(c)
+
+
+
+
+n = int(input())
+n = 8
+print(n // -4 * -1)
+print((n + 4 - 1) // 4)
+
+cm = int(input())
+m = cm // 100
+print(m)
+
+# b1 = int(input())
+# q = int(input())
+# n = int(input())
+#
+# b_n = b1 * q ** (n - 1)
+# print(b_n)
+
+# x = int(input())
+# print(x, 2*x, 3*x, 4*x, 5*x, sep='---')
+
+# sep_ = input()
+# str_1 = input()
+# str_2 = input()
+# str_3 = input()
+# print(str_1, str_2, str_3, sep=sep_)
+#
+# name = input()
+# print('Привет,', name, end='!')
+#
+# name, surname = 'Timur', 'Guev'
+# print('Имя:', name, 'Фамилия:', surname)
+#
+# name, surname = input(), input()
+# print('Имя:', name, 'Фамилия:', surname)
+#
+# # Это комментарий в программе на языке Python.
+# print('Python rocks!')
+#
+# num_1 = int(input())
+# num_2 = int(input())
+# num_3 = int(input())
+# print(num_1 + num_2 + num_3)
+#
+# a = int(input())
+# print('Объем =', a * a * a)
+# print('Площадь полной поверхности =', 6 * a * a)
+
+# a = int(input())
+# b = int(input())
+# print(3 * (a + b) ** 3 + 275 * b ** 2 - 127 * a - 41)
+#
+# num = int(input())
+# print(f'Следующее за числом {num} число: {num + 1}')
+# print(f'Для числа {num} предыдущее число: {num - 1}')
+#
+# price_1 = int(input())
+# price_2 = int(input())
+# price_3 = int(input())
+# price_4 = int(input())
+# print((price_1 + price_2 + price_3 + price_4) * 3)
+#
+# a = int(input())
+# b = int(input())
+#
+# print(f'{a} + {b} = {a + b}')
+# print(f'{a} - {b} = {a - b}')
+# print(f'{a} * {b} = {a * b}')
+
+# a1 = int(input('a1'))
+# d = int(input('d'))
+# n = int(input('n'))
+#
+# a_n = a1 + d * (n - 1)
+# print(a_n)
