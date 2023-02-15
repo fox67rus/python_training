1501839962ee76ee2fcbcfb1f6070020276a695d
Elena <fox67rus@mail.ru>
2023-02-15 19:21:48 +0300

    Модуль "Циклы for и while".

--- course/stepik_pokolenie_1/temp.py
+++ course/stepik_pokolenie_1/temp.py
@@ -1,11 +1,28 @@
-# Простые числа
-a, b = 2, 15
+# Числа Рамануджана 🌶️
+# 1729 =1 ** 3 + 12 ** 3 = 9 ** 3 + 10 ** 3.
 
-for i in range(a, b + 1):
-    flag = True
-    for j in range(2, i):
-        if i % j == 0:
-            flag = False
+import time
+start_time = time.time()
+count = 0
+x = 50
+flag = False
+for a in range(1, x):
+    for b in range(1, x):
+        for c in range(1, x):
+            for d in range(1, x):
+                sum_1 = a ** 3 + b ** 3
+                sum_2 = c ** 3 + d ** 3
+                if sum_1 == sum_2 and a != b and b != c and c != d and a > b and a > d and a > c:
+                    count += 1
+                    print(f'{a=}, {b=}, {c=}, {d=}, {sum_1=}')
+                if count == 10:
+                    flag = True
+                    break
+            if flag:
+                break
+        if flag:
             break
-    if flag and i != 1:
-        print(i)
+    if flag:
+        break
+
+print(f'Время выполнения: {time.time() - start_time}')
